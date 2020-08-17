efdb158b0aec9e90517f3ed20e02b4e6ab85f86e
Aneel <40775413+aneelysis@users.noreply.github.com>
2020-08-17 14:11:15 -0700

    Delete update-sigsci-agent.ps1

--- ansible-playbooks/powershell-scripts/windows-iis/update-sigsci-agent.ps1
+++
@@ -1,7 +0,0 @@
-( new-object System.Net.WebClient ).DownloadFile("https://dl.signalsciences.net/sigsci-agent/sigsci-agent_latest.msi", "c:\\apps\\sigsci\\agent\\sigsci-agent_latest.msi")
-C:\Apps\sigsci\agent\sigsci-agent_latest.msi /quiet
-sleep 60
-net stop sigsci-agent
-net start sigsci-agent
-sleep 30
-iisreset
